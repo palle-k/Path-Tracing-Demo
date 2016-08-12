7700dbeab67325e4aced9f1e0692c13dad2bb099
Palle <palleklewitz@icloud.com>
2016-08-12 13:40:46 +0200

    License Headers

--- PathTracingTests/RenderResultViewController.swift
+++ PathTracingTests/RenderResultViewController.swift
@@ -3,7 +3,24 @@
 //  PathTracingTests
 //
 //  Created by Palle Klewitz on 29.07.16.
-//  Copyright © 2016 Palle Klewitz. All rights reserved.
+//  Copyright © 2016 Palle Klewitz.
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is furnished
+//  to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+//  WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+//  IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 //
 
 import Cocoa
@@ -21,13 +38,13 @@ class RenderResultViewController: NSViewController, PathTracerDelegate
 	func pathTracingDidFinish(render: CGImage)
 	{
 		imageView.image = NSImage(cgImage: render, size: NSSize(width: render.width, height: render.height))
-		self.view.window?.title = "1337 HaXx0r rAyyy(lmao)TraC1n6"
+		self.view.window?.title = "Path Tracing Demo"
 	}
 	
 	func pathTracingDidUpdate(render: CGImage, progress: Float)
 	{
 		imageView.image = NSImage(cgImage: render, size: NSSize(width: render.width, height: render.height))
-		self.view.window?.title = "1337 HaXx0r rAyyy(lmao)TraC1n6 - \(Int(progress * 100))% completed."
+		self.view.window?.title = "Path Tracing Demo - \(Int(progress * 100))% completed."
 	}
 	
 	func pathTracerUpdated(notification: NSNotification?)
--- PathTracingTests/Shading.swift
+++ PathTracingTests/Shading.swift
@@ -3,7 +3,24 @@
 //  PathTracingTests
 //
 //  Created by Palle Klewitz on 01.08.16.
-//  Copyright © 2016 Palle Klewitz. All rights reserved.
+//  Copyright © 2016 Palle Klewitz.
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is furnished
+//  to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+//  WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+//  IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 //
 
 import Foundation
@@ -231,10 +248,6 @@ extension Material: CustomStringConvertible
 	}
 }
 
-
-//Requires class because every triangle needs to hold a reference to a shader.
-//Changes in the shader would otherwise not be applied.
-//Also reduces number of duplicated shaders.
 protocol Shader: class
 {
 	var requiresMultipleSamples:Bool { get }
